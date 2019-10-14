8d4a50616fd762348225a2f7a2a85a8796eeb1e8
ChristineChep <39522563+ChristineChep@users.noreply.github.com>
2019-10-14 13:24:01 +0300

    Add files via upload
    The main activity.java file needs correction kindly help

---
+++ MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.user.sikika4;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.user.sikika4.R;
+
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
+    private DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setNavigationViewListener();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.report){
+                //do somthing
+//                Intent intent = new Intent(MainActivity.this, reportActivity.class);
+//                startActivity(intent);
+//                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+//                drawer.closeDrawer(GravityCompat.START);
+                Toast.openActivity(reportActivity)
+            }
+
+        //close navigation drawer
+
+        mDrawerlayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void setNavigationViewListener() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.report);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+
+    }
