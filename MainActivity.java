b1dcf6aea2535fab05ab1745332c1d624de0c1b0
ChristineChep <39522563+ChristineChep@users.noreply.github.com>
2019-10-24 16:02:39 +0300

    the new updated version

--- MainActivity.java
+++ MainActivity.java
@@ -9,59 +9,94 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.example.user.sikika4.R;
 
-public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener{
+public class MainActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout mDrawerlayout;
-    private ActionBarDrawerToggle mToggle;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
-        mDrawerlayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        setNavigationViewListener();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerlayout, toolbar
+                R.string.open, R.string.close);
+
+
+        mDrawerlayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new dashboardFragment()).commit();
+            navigationView.setCheckedItem(R.id.db);
+        }
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        setNavigationViewListener();
     }
 
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (mToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (mToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        switch (item.getItemId()) {
 
-        if (id == R.id.report){
-                //do somthing
-//                Intent intent = new Intent(MainActivity.this, reportActivity.class);
-//                startActivity(intent);
-//                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
-//                drawer.closeDrawer(GravityCompat.START);
-                Toast.openActivity(reportActivity)
+            case R.id.db:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new dashboardFragment()).commit();
+                break;
+
+//            case R.id.about:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container ,
+//                        new aboutFragment()).commit();
+//                break;
+
+            case R.id.news:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new newsFragment()).commit();
+                break;
+
+            case R.id.report:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new reportFragment()).commit();
+                break;
+
+            case R.id.search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new searchFragment()).commit();
+                break;
+
+
+            mDrawerlayout.closeDrawer(GravityCompat.START);
+            return true;
+        }
+
+
+
+        public void onBackPressed () {
+            if (mDrawerlayout.isDrawerOpen(GravityCompat.START)) {
+                mDrawerlayout.closeDrawer(GravityCompat.START);
             }
-
-        //close navigation drawer
-
-        mDrawerlayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    private void setNavigationViewListener() {
-        NavigationView navigationView = (NavigationView) findViewById(R.id.report);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-
-    }
+            else {
+                super.onBackPressed();
+            }
+                    }
